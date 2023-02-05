c8db3bc4806d5e6339fc0a77ac2ef828aaaa70e1
sanchitmhadgut <96831168+sanchitmhadgut@users.noreply.github.com>
2023-02-05 07:37:21 +0530

    Update led_brightness.py

--- led_brightness.py
+++ led_brightness.py
@@ -7,6 +7,7 @@ pot = ADC(Pin(33))
 pot.width(ADC.WIDTH_12BIT)
 pot.atten(ADC.ATTN_11DB)
 
+
 while True:
     pot_value = pot.read()
     print(pot_value)
@@ -14,6 +15,6 @@ while True:
     if pot_value < 15:
         led.duty(0)
     else:
-        led.duty(pot_value)
+        led.duty(int(pot_value * 1023 / 4095))
         
     time.sleep_ms(5)    
